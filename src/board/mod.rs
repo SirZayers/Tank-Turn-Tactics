ccfa4b7d52c688f013632dbaf6ef88089691ac83
K. Kraus <laqueray@googlemail.com>
2022-08-16 07:34:45 +0200

    structs and code for basic actions

--- src/board/mod.rs
+++ src/board/mod.rs
@@ -1,32 +1,34 @@
+mod action;
 mod player;
 
-use std::io::Write;
-use std::{collections::HashMap};
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 use std::fmt;
 use std::fmt::Write as fmtWrite;
-use serde::{Deserialize, Serialize};
 use std::fs::OpenOptions;
+use std::io::Write;
 
-use crate::board::player::Player;
-
+pub use crate::board::action::{act, Action, ClientPlayer, Direction, Request, Response};
+pub use crate::board::player::Player;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Board {
     id: u128,
-    width: u64,
-    height: u64,
+    width: i64,
+    height: i64,
     logfile: String,
     lastlog: bool,
     has_started: bool,
     has_ended: bool,
     winner: Option<Player>,
-    players: HashMap<u128, Player>
+    pub players: HashMap<u128, Player>,
 }
 
 impl fmt::Display for Board {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match write!(f,
-        "BOARD\n
+        match write!(
+            f,
+            "BOARD\n
         id: {}\n
         height: {}\n
         width: {}\n
@@ -34,43 +36,62 @@ impl fmt::Display for Board {
         has_started: {}\n
         has_ended: {}\n
         winner: {:?}\n",
-    self.id, self.width, self.height, &self.logfile, self.has_started, self.has_ended, &self.winner) {
-        Ok(_) => (),
-        Err(e) => return Err(e)
-    };
-
-    for (_, player) in &self.players {
-        match writeln!(format!("{player}")) {
+            self.id,
+            self.width,
+            self.height,
+            &self.logfile,
+            self.has_started,
+            self.has_ended,
+            &self.winner
+        ) {
             Ok(_) => (),
-            Err(e) => return Err(e)
+            Err(e) => return Err(e),
         };
-    }
-    Ok(())
+
+        for (_, player) in &self.players {
+            match writeln!(format!("{player}")) {
+                Ok(_) => (),
+                Err(e) => return Err(e),
+            };
+        }
+        Ok(())
     }
 }
 
-
-
-pub fn create_board (id: u128, width: u64, height: u64, logfilepath: &String) -> Result<Board, String> {
-
+pub fn create_board(
+    id: u128,
+    width: i64,
+    height: i64,
+    logfilepath: &String,
+) -> Result<Board, String> {
     // Create two logfiles named <path>.id.(1/2).log if they don't exist yet
 
     let mut logfile0 = match OpenOptions::new()
-    .write(true)
-    .create_new(true)
-    .open(format!("{}.{}.0.log", logfilepath, id)) {
-        Err(_) => return Err(format!("Error: Logfile {}.{}.0.log couldn't be created or already exists.", logfilepath, id)),
-        Ok(f) => f
+        .write(true)
+        .create_new(true)
+        .open(format!("{}.{}.0.log", logfilepath, id))
+    {
+        Err(_) => {
+            return Err(format!(
+                "Error: Logfile {}.{}.0.log couldn't be created or already exists.",
+                logfilepath, id
+            ))
+        }
+        Ok(f) => f,
     };
 
-
     let mut logfile1 = match OpenOptions::new()
-    .write(true)
-    .create_new(true)
-    .open(format!("{}.{}.1.log", logfilepath, id)) {
-        Err(_) => return Err(format!("Error: Logfile {}.{}.1.log couldn't be created or already exists.", logfilepath, id)),
-        Ok(f) => f
-    
+        .write(true)
+        .create_new(true)
+        .open(format!("{}.{}.1.log", logfilepath, id))
+    {
+        Err(_) => {
+            return Err(format!(
+                "Error: Logfile {}.{}.1.log couldn't be created or already exists.",
+                logfilepath, id
+            ))
+        }
+        Ok(f) => f,
     };
 
     let newboard = Board {
@@ -82,22 +103,56 @@ pub fn create_board (id: u128, width: u64, height: u64, logfilepath: &String) ->
         has_ended: false,
         has_started: false,
         winner: None,
-        players: HashMap::new()
+        players: HashMap::new(),
     };
 
     // Fill log files with board information
 
     match logfile0.write_all(serde_json::to_string(&newboard).unwrap().as_bytes()) {
-        Err(_) => return Err(format!("Error: write to {}.{}.0.log failed.", logfilepath, id)),
-        Ok(_) => ()
+        Err(_) => {
+            return Err(format!(
+                "Error: write to {}.{}.0.log failed.",
+                logfilepath, id
+            ))
+        }
+        Ok(_) => (),
     };
 
     match logfile1.write_all(serde_json::to_string(&newboard).unwrap().as_bytes()) {
-        Err(_) => return Err(format!("Error: write to {}.{}.1.log failed.", logfilepath, id)),
-        Ok(_) => ()
+        Err(_) => {
+            return Err(format!(
+                "Error: write to {}.{}.1.log failed.",
+                logfilepath, id
+            ))
+        }
+        Ok(_) => (),
     };
 
     return Ok(newboard);
 }
 
+pub fn create_local_board(id: u128, width: i64, height: i64) -> Board {
+    return Board {
+        id,
+        width,
+        height,
+        logfile: String::from(""),
+        lastlog: true,
+        has_ended: false,
+        has_started: false,
+        winner: None,
+        players: HashMap::new(),
+    };
+}
 
+pub fn update_player(board: &mut Board, player: Player) {
+    board.players.insert(player.token, player);
+}
+
+pub fn grant_action(board: &mut Board, player_token: u128) {
+    let mut sender = match board.players.get_mut(&player_token) {
+        None => return,
+        Some(sender) => sender,
+    };
+    sender.action_points += 1;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,85 @@
 mod board;
 
+use board::{
+    act, create_local_board, grant_action, update_player, Action,
+    Action::{Donate, Log, Players, TankMove, TankShoot, TankUpgrade},
+    Board,
+    Direction::{Down, Left, Right, Up},
+    Player, Request, Response,
+};
 
 // Create board an save in HashMap by board id
 
-fn main() {
-    let board = board::create_board(1, 10, 10, &String::from("./log")).unwrap();
+fn serve(board: &mut Board, sender_token: u128, action: Action) -> Response {
+    let request = Request {
+        sender_token,
+        action,
+    };
+    let response = act(board, &request.sender_token, &request.action);
+    let request_json = serde_json::to_string(&request).unwrap();
+    let response_json = serde_json::to_string(&response).unwrap();
+    println!("{:}", request_json);
+    println!("\t-> {:}", response_json);
+    println!("{:?}\n", board);
+    response
+}
 
-    println!("Test board:\n{:?}", board);
+fn main() {
+    let mut board = create_local_board(1, 10, 10);
+    update_player(
+        &mut board,
+        Player {
+            token: 1,
+            nickname: String::from("p1"),
+            contact: String::from("p1@localhost"),
+            position: (0, 0),
+            action_points: 1,
+            hit_points: 3,
+            range: 2,
+        },
+    );
+    update_player(
+        &mut board,
+        Player {
+            token: 2,
+            nickname: String::from("p2"),
+            contact: String::from("p2@localhost"),
+            position: (5, 5),
+            action_points: 1,
+            hit_points: 3,
+            range: 2,
+        },
+    );
+    println!("{:?}\n", board);
+
+    grant_action(&mut board, 1);
+    grant_action(&mut board, 1);
+    grant_action(&mut board, 1);
+    grant_action(&mut board, 2);
+    grant_action(&mut board, 2);
+    grant_action(&mut board, 2);
+    println!("{:?}\n", board);
+
+    serve(&mut board, 1, Action::Players);
+    assert_eq!(
+        serve(&mut board, 1, TankMove { direction: Up }),
+        Response::Ok
+    );
+    assert_eq!(
+        serve(&mut board, 1, TankMove { direction: Up }),
+        Response::Ok
+    );
+    assert_eq!(
+        serve(&mut board, 1, TankMove { direction: Left }),
+        Response::MovesOutside,
+    );
+    assert_eq!(
+        serve(&mut board, 1, TankMove { direction: Right }),
+        Response::Ok
+    );
+
+    assert_eq!(
+        serve(&mut board, 2, TankShoot { target_token: 1 }),
+        Response::Ok
+    );
 }
